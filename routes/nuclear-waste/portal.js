80e60d941ccd35dae8f91b49684bacf94c7cab32
yglin <yglin.mlanser@gmail.com>
2015-10-15 09:27:00 +0800

    Add to portal: uploadUrl

--- routes/nuclear-waste/portal.js
+++ routes/nuclear-waste/portal.js
@@ -8,9 +8,10 @@ router.get('/', function(req, res, next) {
         name: 'nuclear-waste',
         title: process.env.NODE_ENV == 'production' ? '核廢料掩埋場' : '核廢料掩埋場(DEV)',
         intro: '<h3>就是個丟棄核廢料的地方，百無禁忌</h3><br><h5>注意：這是測試用的電台，所以文章不定時會清除，最多保留三個月</h5>',
-        portalUrl: req.protocol + '://' + req.get('host') + req.originalUrl,
-        postUrl: req.protocol + '://' + req.get('host') + req.originalUrl.replace('portal', 'posts'),
-        commentUrl: req.protocol + '://' + req.get('host') + req.originalUrl.replace('portal', 'posts/:post_id/comments'),
+        portalUrl: req.protocol + '://' + req.get('host') + '/nuclear-waste/portal',
+        postUrl: req.protocol + '://' + req.get('host') + '/nuclear-waste/posts',
+        commentUrl: req.protocol + '://' + req.get('host') + '/nuclear-waste/posts/:post_id/comments',
+        uploadUrl: req.protocol + '://' + req.get('host') + '/nuclear-waste/upload',
         logo: 'https://yt3.ggpht.com/-Gd9lF_AqQPk/AAAAAAAAAAI/AAAAAAAAAAA/afbtVZjs18E/s88-c-k-no/photo.jpg',
         iconSet: {
             sweat: 'http://findicons.com/files/icons/2020/2s_space_emotions/128/sweat.png',
@@ -35,7 +36,7 @@ router.get('/', function(req, res, next) {
             cool: 'http://findicons.com/files/icons/2020/2s_space_emotions/128/cool.png'
         },
         followPostBy: ['modify_time'],
-        statisticUrl: req.protocol + '://' + req.get('host') + req.originalUrl.replace('portal', 'posts/:post_id/statistics'),
+        statisticUrl: req.protocol + '://' + req.get('host') + '/nuclear-waste/posts/:post_id/statistics',
     };
     res.json(portalData);
 });
