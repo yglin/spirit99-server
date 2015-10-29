25c74bc49480a4cc6bdcee1b1be6b4f2d9ca8250
yglin <yglin.mlanser@gmail.com>
2015-10-29 14:08:23 +0800

    Issue report: save error_logs as json string

--- routes/spirit99.js
+++ routes/spirit99.js
@@ -8,6 +8,7 @@ router.post('/issues', function (req, res) {
     var issueData = req.body;
     issueData.client_ip = req.clientIp;
     issueData.user_agent = req.headers['user-agent'];
+    issueData.error_logs = JSON.stringify(req.body.error_logs);
 
     // console.log(issueData);
     req.app.mysqlPoolCluster.getConnection('spirit99', function (error, connection) {
@@ -17,7 +18,7 @@ router.post('/issues', function (req, res) {
             res.send(error);
         }
         else{
-            var sql = mysql.format('INSERT INTO `issue` SET ?', [req.body]);
+            var sql = mysql.format('INSERT INTO `issue` SET ?', [issueData]);
             // console.log(sql);
             connection.query(sql,
             function (error, result){
@@ -53,6 +54,9 @@ router.get('/issues', function (req, res) {
                     res.send(error);                    
                 }
                 else{
+                    results.forEach(function (issue, index) {
+                        issue.error_logs = JSON.parse(issue.error_logs);
+                    });
                     res.json(results);
                 }
             })
